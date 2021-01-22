f2e4613315670f5316bdbaa51475a22e3465c1a9
Alan Call <alanmatthewcall@gmail.com>
2021-01-22 12:25:52 -0500

    add js for Beep

--- js/styles.js
+++ js/styles.js
@@ -1,12 +1,24 @@
-Describe: replacesDigits()
-Test: "It replaces the number 1 with Beep"
-Expect(isBeep(1)).toEqual(true);
+//Describe: replacesDigits()
+//Test: "It replaces the number 1 with Beep"
+//Expect(isBeep(1)).toEqual(true);
 
-Test: "It replaces the number 2 with Boop"
-Expect(isBoop(2)).toEqual(true);
+//Test: "It replaces the number 2 with Boop"
+//Expect(isBoop(2)).toEqual(true);
 
-Test: "It replaces the number 3 with Won't you be my neighbor?"
-Expect(isWontYouBeMyNeighbor(3)).toEqual(true);
+//Test: "It replaces the number 3 with Won't you be my neighbor?"
+//Expect(isWontYouBeMyNeighbor(3)).toEqual(true);
 
-Test: "It replaces the number 2 with Boop"
-Expect(isBoop(2)).toEqual(true);
+//Test: "It returns range of numbers up to inputted number"
+//Expect(0, Beep, Boop, Wont you be my neighbor, 4, 5(5)).toEqual(true);
+
+$(document).ready(function() {
+  $("form#form-one").submit(function(event) {
+  });
+});
+    
+
+  const numberOne = 1;
+  const Beep! = parseInt(numberOne);
+
+  event.preventDefault();
+
