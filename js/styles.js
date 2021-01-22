e847b377c393e753d7773d9281ce0e74475fd314
Alan Call <alanmatthewcall@gmail.com>
2021-01-22 11:47:59 -0500

    add form, submit button adn test's

---
+++ js/styles.js
@@ -0,0 +1,12 @@
+Describe: replacesDigits()
+Test: "It replaces the number 1 with Beep"
+Expect(isBeep(1)).toEqual(true);
+
+Test: "It replaces the number 2 with Boop"
+Expect(isBoop(2)).toEqual(true);
+
+Test: "It replaces the number 3 with Won't you be my neighbor?"
+Expect(isWontYouBeMyNeighbor(3)).toEqual(true);
+
+Test: "It replaces the number 2 with Boop"
+Expect(isBoop(2)).toEqual(true);
